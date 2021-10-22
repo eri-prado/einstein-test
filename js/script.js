0adc601fce97fd779fa9893008c946ba2b247d39
Erinaldo Prado <eri.prado@hotmail.com>
2021-10-22 19:52:32 -0300

    changing color of houses

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const houseColors = document.querySelectorAll('.house');
-console.log(houseColors)
-const arrayColors = Array.from(houseColors)
-console.log(arrayColors)
+function change(list) {
+  var optionValue = list.options[list.selectedIndex].value
+  var house = document.querySelector(".house")
+  house.style.background = optionValue
+}
