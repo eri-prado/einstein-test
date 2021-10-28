d7e82c406c71411f053475b7a786d2475f081306
Erinaldo Prado <eri.prado@hotmail.com>
2021-10-28 19:30:12 -0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,44 @@
-function change(list) {
-  var optionValue = list.options[list.selectedIndex].value
-  var house = document.querySelector(".house")
-  house.style.background = optionValue
+function selectColor(list) {
+  const houses = document.querySelectorAll(".house");
+  const optionValue = list.options[list.selectedIndex].value
+
+
+  function change(item) {
+    [].forEach.call(houses, house => {
+      if (house.className === optionValue) {
+        house.classList.toggle(optionValue)
+      }
+    })
+    item.classList.toggle(optionValue);
+  }
+
+  [].forEach.call(houses, house => {
+    house.addEventListener("change", function () {
+      change(this, optionValue)
+    })
+  })
 }
+
+
+
+
+  // if (optionValue == "yellow") {
+  //   house.classList.add('yellow')
+
+  // for (var i = 0; i < house.length; ++i) {
+  //   var item = house[i]
+  //   item.style.background = optionValue
+  // }
+
+  // house.style.background = optionValue
+
+// function change(listColors) {
+//   var optionValue = listColors.options[listColors.selectedIndex].value
+//   var house = document.querySelectorAll(".house")
+//   for (var i = 0; i < house.length; ++i) {
+//     var item = house[i]
+//     item.style.background = optionValue
+//   }
+
+// }
+
